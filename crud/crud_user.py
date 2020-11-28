688dfb5b8f5812069d45fb5007d5eef3f809113e
nazerpanahi <nazerpanahi@gmail.com>
2020-11-28 11:31:38 +0330

    delete the db/crud and add new crud module and crud_user.py

---
+++ crud/crud_user.py
@@ -0,0 +1,33 @@
+from sqlalchemy.orm import Session
+
+from db.modelcrud import ModelCRUD
+from models import User
+from schemas import UserCreate
+from security import get_password_hash
+
+
+class UserCRUD(ModelCRUD):
+    def __init__(self, users_db: Session):
+        super().__init__(User, users_db)
+
+    def get_by_id(self, user_id: int):
+        return self.get_first(User.user_id == user_id)
+
+    def get_by_username(self, username: str):
+        return self.get_first(User.username == username)
+
+    def add_new_user(self, user: UserCreate):
+        super().add_new(username=user.username, first_name=user.first_name, last_name=user.last_name,
+                        password=get_password_hash(user.password))
+
+    def delete_by_id(self, user_id: int):
+        return self.delete(User.user_id == user_id)
+
+    def delete_by_username(self, username: str):
+        return self.delete(User.username == username)
+
+    def update_by_id(self, user_id: int, values: dict):
+        return self.update(values, User.user_id == user_id)
+
+    def update_by_username(self, username: str, values: dict):
+        return self.update(values, User.username == username)
