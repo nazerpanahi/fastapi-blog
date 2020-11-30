6f96dc400bc8bf76462ead243a67e471f250dd50
nazerpanahi <nazerpanahi@gmail.com>
2020-11-30 10:04:55 +0330

    register new routers in routers.py

--- conf/routers.py
+++ conf/routers.py
@@ -1,5 +1,7 @@
-from routers import auth_router
+from routers import auth_router, post_router, user_post_like_router
 
 ROUTERS = [
-    {'router': auth_router},
+    {'router': auth_router, 'tags': ['Authentication']},
+    {'router': post_router, 'prefix': '/post', 'tags': ['Post']},
+    {'router': user_post_like_router}
 ]
