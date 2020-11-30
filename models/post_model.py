58b2fe593f0986a7f6f03fb160564572d936047a
nazerpanahi <nazerpanahi@gmail.com>
2020-11-30 09:44:28 +0330

    add Post model

---
+++ models/post_model.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime, Text
+
+from conf.constants import db_user_table_name, db_post_table_name
+from db.database import SQL_Base, SE_Base
+
+
+class Post(SQL_Base, SE_Base):
+    __tablename__ = db_post_table_name
+
+    post_id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    content = Column(Text)
+    created_at = Column(DateTime, default=datetime.now())
+    author_id = Column(Integer, ForeignKey(f"{db_user_table_name}.user_id"))
