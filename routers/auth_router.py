9c62dca80ddf4fedbe6d07025deea32329abf14e
nazerpanahi <nazerpanahi@gmail.com>
2020-11-30 10:03:45 +0330

    move db.utils.py to utils package and refactor auth_router.py

--- routers/auth_router.py
+++ routers/auth_router.py
@@ -1,38 +1,41 @@
 from fastapi import APIRouter, Request, Depends, Response
+from redis import Redis
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session
 
-from auth import auth_utils, token_utils
+from conf.api import auth_apis
 from conf.constants import default_token_expire_minutes
-from conf.errors import KnownErrors
+from core.errors import KnownErrors
+from core.responses import ok_response, nok_response
 from crud import crud_user
-from db.utils import get_sql_db, get_redis_db
 from schemas import UserCreate
+from utils import auth_utils, token_utils
+from utils.db_utils import get_sql_db, get_redis_db
 
 router = APIRouter()
 
 
-@router.post('/register', tags=['Authentication'], name='register')
+@router.post(auth_apis.get('register'), name='register')
 def register(request: Request,
              user: UserCreate,
              users_db: Session = Depends(get_sql_db),
-             tokens_db=Depends(get_redis_db)):
-    access_token = auth_utils.is_authenticated(request, tokens_db)  # Return None if the user is not authenticated
+             tokens_db: Redis = Depends(get_redis_db)):
+    access_token = auth_utils.is_authenticated(request, tokens_db)
     if access_token is None:  # request is not authorized
         try:
             user_db = crud_user.UserCRUD(users_db=users_db).add_new_user(user)
-            return user_db
+            return ok_response()
         except IntegrityError:
             raise KnownErrors.ERROR_USER_EXISTS
     else:
         raise KnownErrors.ERROR_BAD_REQUEST
 
 
-@router.post('/login', tags=['Authentication'], name='login')
+@router.post(auth_apis.get('login'), name='login')
 def login(request: Request,
           user: UserCreate,
           users_db: Session = Depends(get_sql_db),
-          tokens_db=Depends(get_redis_db)):
+          tokens_db: Redis = Depends(get_redis_db)):
     access_token = auth_utils.is_authenticated(request, tokens_db)  # Return None if the user is not authenticated
     if access_token is None:  # request is not authorized
         access_token = auth_utils.login_for_access_token(user.username,
@@ -44,14 +47,21 @@ def login(request: Request,
     return resp
 
 
-@router.post('/logout', tags=['Authentication'], name='logout')
+@router.post(auth_apis.get('logout'), name='logout')
 def logout(request: Request,
-           tokens_db=Depends(get_redis_db)):
-    access_token = auth_utils.is_authenticated(request, tokens_db)  # Return None if the user is not authenticated
-    if access_token is None:  # request is not authorized
-        return KnownErrors.ERROR_BAD_REQUEST
-    b = token_utils.delete_token(token=access_token, tokens_db=tokens_db)
-    if b:
-        return {"message": "OK"}
+           tokens_db: Redis = Depends(get_redis_db)):
+    access_token = auth_utils.is_authenticated(request, tokens_db, error=KnownErrors.ERROR_BAD_REQUEST)
+    is_deleted = token_utils.delete_token(token=access_token, tokens_db=tokens_db)
+    if is_deleted:
+        return ok_response()
     else:
-        return {"message": "NOK"}
+        return nok_response()
+
+
+@router.post(auth_apis.get('delete_account'), name='delete_account')
+def delete_account(request: Request,
+                   users_db: Session = Depends(get_sql_db),
+                   tokens_db: Redis = Depends(get_redis_db)):
+    access_token = auth_utils.is_authenticated(request, tokens_db, error=KnownErrors.ERROR_BAD_REQUEST)
+    resp = auth_utils.delete_user_account(access_token, users_db=users_db)
+    return ok_response(data=resp)
