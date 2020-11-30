64c50b2f5802736513863559aa71f376938eb873
nazerpanahi <nazerpanahi@gmail.com>
2020-11-30 09:45:16 +0330

    add UserPostLike model

---
+++ models/user_post_like_model.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, ForeignKey, Integer
+
+from conf.constants import db_user_post_like_table_name, db_user_table_name, db_post_table_name
+from db.database import SQL_Base, SE_Base
+
+
+class UserPostLike(SQL_Base, SE_Base):
+    __tablename__ = db_user_post_like_table_name
+
+    post_id = Column(Integer, ForeignKey(f"{db_post_table_name}.post_id"), primary_key=True)
+    user_id = Column(Integer, ForeignKey(f"{db_user_table_name}.user_id"), primary_key=True)
