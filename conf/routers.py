50b827fd2b403a953bd11b370f344486bb60b0f9
nazerpanahi <nazerpanahi@gmail.com>
2020-12-02 12:14:52 +0330

    register comment routers

--- conf/routers.py
+++ conf/routers.py
@@ -1,7 +1,8 @@
-from routers import auth_router, post_router, user_post_like_router
+from routers import auth_router, post_router, user_post_like_router, comment_router
 
 ROUTERS = [
     {'router': auth_router, 'tags': ['Authentication']},
     {'router': post_router, 'prefix': '/post', 'tags': ['Post']},
-    {'router': user_post_like_router}
+    {'router': user_post_like_router, 'prefix': '/post', 'tags': ['Like', 'Post']},
+    {'router': comment_router, 'prefix': '/comment', 'tags': ['Comment', 'Post']}
 ]
