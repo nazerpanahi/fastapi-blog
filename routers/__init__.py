9f7ad7304b9136732508f3c08dba3e29e6d70df1
nazerpanahi <nazerpanahi@gmail.com>
2020-12-02 12:14:32 +0330

    export comment router to other packages

--- routers/__init__.py
+++ routers/__init__.py
@@ -1,3 +1,4 @@
 from .auth_router import router as auth_router
 from .post_router import router as post_router
 from .user_post_like_router import router as user_post_like_router
+from .comment_router import router as comment_router
