089b54253ead62c4cc0884b8f4ba6c0d2a727293
nazerpanahi <nazerpanahi@gmail.com>
2020-12-02 09:34:38 +0330

    add comment model

---
+++ models/comment_model.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+from sqlalchemy import Column, ForeignKey, Integer, Date, Text
+
+from conf.constants import db_user_table_name, db_post_table_name, db_comment_table_name
+from db.database import SQL_Base, SE_Base
+
+
+class Comment(SQL_Base, SE_Base):
+    __tablename__ = db_comment_table_name
+
+    comment_id = Column(Integer, primary_key=True, index=True)
+    content = Column(Text, nullable=False)
+    created_at = Column(Date, default=datetime.now().date())
+    author_id = Column(Integer, ForeignKey(f"{db_user_table_name}.user_id"))
+    post_id = Column(Integer, ForeignKey(f"{db_post_table_name}.post_id"))
