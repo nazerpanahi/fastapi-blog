8939348917a3a15e50e3b568210c3c0c39998425
nazerpanahi <nazerpanahi@gmail.com>
2020-12-08 11:51:40 +0330

    add 'save_to_elasticsearch_db' function as a task

--- broker/tasks.py
+++ broker/tasks.py
@@ -1,4 +1,13 @@
 from __future__ import absolute_import, unicode_literals
 
-from broker.celery import app
+from elasticsearch import Elasticsearch
 
+from broker.celery import app
+from conf.settings import ELASTICSEARCH_SETTINGS
+
+db = Elasticsearch(hosts=ELASTICSEARCH_SETTINGS['hosts'])
+
+@app.task()
+def save_data_in_elastic(data: dict, index='default'):
+    db.index(index=index, body=data)
+    return data
