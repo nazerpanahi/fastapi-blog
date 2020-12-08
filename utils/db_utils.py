660a0032c5cb04867268c46ff8a60f0788fcaae8
nazerpanahi <nazerpanahi@gmail.com>
2020-12-08 11:47:15 +0330

    delete get_search_engine_db function

--- utils/db_utils.py
+++ utils/db_utils.py
@@ -1,4 +1,4 @@
-from db.database import SQL_SessionLocal, SE_SessionLocal, REDIS_CONNECTION
+from db.database import SQL_SessionLocal, REDIS_CONNECTION
 
 
 def get_sql_db():
@@ -7,14 +7,6 @@ def get_sql_db():
         yield sql_db
     finally:
         sql_db.close()
-
-
-def get_search_engine_db():
-    se_db = SE_SessionLocal()
-    try:
-        yield se_db
-    finally:
-        se_db.close()
 
 
 def get_redis_db():
