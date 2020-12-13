75567789462396c917ff94423ec6b3bfe3d00aa1
nazerpanahi <nazerpanahi@gmail.com>
2020-12-13 09:40:28 -0500

    refactor errors.py and constants.py

--- conf/constants.py
+++ conf/constants.py
@@ -1,3 +1,5 @@
+from fastapi import status
+
 default_token_expire_minutes = 15
 token_header_key = 'Authorization'
 
@@ -7,22 +9,47 @@ db_user_table_name = f"{db_table_prefix}users"
 db_post_table_name = f"{db_table_prefix}posts"
 db_user_post_like_table_name = f"{db_table_prefix}user_post_like"
 db_comment_table_name = f"{db_table_prefix}comments"
-# db_token_table_name = f"{db_table_prefix}tokens"
 
-error_user_exists = 'User exists'
-error_user_not_exists = 'User not exists'
-error_not_authenticated = 'Not Authenticated'
-error_incorrect_username = 'Incorrect username or password'
-error_incorrect_password = 'Incorrect username or password'
-error_not_valid_credentials = 'Could not validate credentials'
-error_user_not_found = 'User not found'
-error_bad_request = 'Bad Request'
+error_user_exists = 'error_user_exists'
+error_user_not_exists = 'error_user_not_exists'
+error_not_authenticated = 'error_not_authenticated'
+error_incorrect_username = 'error_incorrect_username'
+error_incorrect_password = 'error_incorrect_password'
+error_not_valid_credentials = 'error_not_valid_credentials'
+error_user_not_found = 'error_user_not_found'
+error_bad_request = 'error_bad_request'
 
-error_user_exists_code = 400
-error_user_not_exists_code = 400
-error_not_authenticated_code = 400
-error_incorrect_username_code = 400
-error_incorrect_password_code = 400
-error_not_valid_credentials_code = 400
-error_user_not_found_code = 404
-error_bad_request_code = 400
+errors_settings = {
+    error_user_exists: {
+        'status_code': status.HTTP_400_BAD_REQUEST,
+        'detail': 'User exists',
+    },
+    error_user_not_exists: {
+        'status_code': status.HTTP_400_BAD_REQUEST,
+        'detail': 'User not exists',
+    },
+    error_not_authenticated: {
+        'status_code': status.HTTP_400_BAD_REQUEST,
+        'detail': 'Not Authenticated',
+    },
+    error_incorrect_username: {
+        'status_code': status.HTTP_400_BAD_REQUEST,
+        'detail': 'Incorrect username or password',
+    },
+    error_incorrect_password: {
+        'status_code': status.HTTP_400_BAD_REQUEST,
+        'detail': 'Incorrect username or password',
+    },
+    error_not_valid_credentials: {
+        'status_code': status.HTTP_400_BAD_REQUEST,
+        'detail': 'Could not validate credentials',
+    },
+    error_user_not_found: {
+        'status_code': status.HTTP_422_UNPROCESSABLE_ENTITY,
+        'detail': 'User not found',
+    },
+    error_bad_request: {
+        'status_code': status.HTTP_400_BAD_REQUEST,
+        'detail': 'Bad Request',
+    },
+}
--- core/errors.py
+++ core/errors.py
@@ -4,19 +4,11 @@ from conf import constants
 
 
 class KnownErrors:
-    ERROR_USER_EXISTS = HTTPException(status_code=constants.error_user_exists_code,
-                                      detail=constants.error_user_exists)
-    ERROR_USER_NOT_EXISTS = HTTPException(status_code=constants.error_user_not_exists_code,
-                                          detail=constants.error_user_not_exists)
-    ERROR_NOT_AUTHENTICATED = HTTPException(status_code=constants.error_not_authenticated_code,
-                                            detail=constants.error_not_authenticated)
-    ERROR_INCORRECT_USERNAME = HTTPException(status_code=constants.error_incorrect_username_code,
-                                             detail=constants.error_incorrect_username)
-    ERROR_INCORRECT_PASSWORD = HTTPException(status_code=constants.error_incorrect_password_code,
-                                             detail=constants.error_incorrect_password)
-    ERROR_NOT_VALID_CREDENTIALS = HTTPException(status_code=constants.error_not_valid_credentials_code,
-                                                detail=constants.error_not_valid_credentials)
-    ERROR_USER_NOT_FOUND = HTTPException(status_code=constants.error_user_not_found_code,
-                                         detail=constants.error_user_not_found)
-    ERROR_BAD_REQUEST = HTTPException(status_code=constants.error_bad_request_code,
-                                      detail=constants.error_bad_request)
+    ERROR_USER_EXISTS = HTTPException(**constants.errors_settings[constants.error_user_exists])
+    ERROR_USER_NOT_EXISTS = HTTPException(**constants.errors_settings[constants.error_user_not_exists])
+    ERROR_NOT_AUTHENTICATED = HTTPException(**constants.errors_settings[constants.error_not_authenticated])
+    ERROR_INCORRECT_USERNAME = HTTPException(**constants.errors_settings[constants.error_incorrect_username])
+    ERROR_INCORRECT_PASSWORD = HTTPException(**constants.errors_settings[constants.error_incorrect_password])
+    ERROR_NOT_VALID_CREDENTIALS = HTTPException(**constants.errors_settings[constants.error_not_valid_credentials])
+    ERROR_USER_NOT_FOUND = HTTPException(**constants.errors_settings[constants.error_user_not_found])
+    ERROR_BAD_REQUEST = HTTPException(**constants.errors_settings[constants.error_bad_request])
